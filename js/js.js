988fcf71815cd01f9151dfa68827fd462f99a25e
Radian <craziestdavid@gmail.com>
2016-04-29 08:00:19 +0800

    init

---
+++ js/js.js
@@ -0,0 +1,8 @@
+var todoInputEle    = document.getElementById("todo-input") ;
+var submitBtnEle    = document.getElementById("todo-submit") ;
+
+var todoListEle     = document.getElementById("todo-todoList") ;
+var validateHintEle = document.getElementById("hint") ;
+
+
+var todoDataArray = [] ;
